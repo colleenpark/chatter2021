af7ec312520784f222c74dc7ea3e4a949b8a7088
stlp <gunh9801@uw.edu>
2021-01-25 19:30:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+    </header>
+  </div>
+}
+export default App;
