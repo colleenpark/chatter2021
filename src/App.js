525dba0714b5fc8ac05c8980d8faf4703c1e2e5d
stlp <gunh9801@uw.edu>
2021-02-01 21:16:09 -0800

    my changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import TextInput from './TextInput';
+import Message from './Messages'
+
 function App() {
+  const [messages,setMessages] = useState([])
+
   return <div className="App">
+ 
     <header className="header">
       <div className="logo" />
       CHATTER
     </header>
+
+    <main className="messages">
+      {messages.map((m,i)=> {
+        return <Message key={i} {...m} />
+      })}
+    </main>
+
+    <TextInput 
+      send={(t)=> setMessages( [{text:t}, ...messages] )}
+    />
+
   </div>
 }
+
 export default App;
